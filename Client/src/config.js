a09c231955e9abe194537ab3e1711bafd0d005b4
chetan9129 <anku9129@gmail.com>
2023-07-31 22:26:19 +0530

    AmplifAI Update

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB2V4vkfv3D2ETV3M_YmbmpOs7sWxpxZR4",
-  authDomain: "login-c76f4.firebaseapp.com",
-  projectId: "login-c76f4",
-  storageBucket: "login-c76f4.appspot.com",
-  messagingSenderId: "839020756839",
-  appId: "1:839020756839:web:c045599d5c5768da6fb9c2",
+  apiKey: "AIzaSyDVYO2sNKRAvFoxHbNS6-_-c-iEkO0M7lw",
+  authDomain: "amplifai-52311.firebaseapp.com",
+  projectId: "amplifai-52311",
+  storageBucket: "amplifai-52311.appspot.com",
+  messagingSenderId: "965178414121",
+  appId: "1:965178414121:web:41bf636975b65801eb7838",
 };
 
 // Initialize Firebase
